7d90d96cef7064a99624695f146f5a8bf5e6839b
Ben Wunderlich <ben.wunderlich@outloo.com>
2020-08-12 22:18:55 -0700

    removing unnecessary button
    adding title to popup
    removing unused file

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ var papaURL;
 function addNoteSpace() {
     document.getElementById("noteSpace").style.display = "inline";
     noteFromStorage();
-    document.getElementById("addNote").style.display = "none";
+    document.getElementById("loadingMessage").style.display = "none";
 }
 
 function urlHasNote(url){
@@ -27,13 +27,23 @@ function closeNote(){
     window.close();
 }
 
+function KeySave(e){
+    if(e.keyCode===13){
+        saveNote();
+        console.log("we workin");
+    }
+}
+
 function getURL(){
     chrome.runtime.sendMessage({greeting: "GetURL"},
     function (response) {
         tabURL = response.navURL;
         papaURL = tabURL;
+        addNoteSpace();
         if(urlHasNote(papaURL)){
-            document.getElementById("addNote").innerHTML = "edit note"
+            noteFromStorage();
+
+            //document.getElementById("addNote").innerHTML = "edit note"
         }
         //$("#tabURL").text(tabURL);
     });
@@ -46,3 +56,16 @@ function deleteNote(){
     }
     closeNote();
 }
+
+//EVENT LISTENERS
+//document.getElementById("addNote").addEventListener("click", addNoteSpace);
+
+document.getElementById("saveButton").addEventListener("click", saveNote);
+
+document.getElementById("cancelButton").addEventListener("click", closeNote);
+
+document.getElementById("delbutton").addEventListener("click", deleteNote);
+
+document.getElementById("noteSpace").addEventListener("keydown", KeySave);
+
+getURL()
